fd2e3749502c2fd462f7021074130a959a8d3301
Asiishabee <42670433+Asiishabee@users.noreply.github.com>
2019-02-19 11:02:54 +0530

    Update cab.py

--- cab.py
+++ cab.py
@@ -1,57 +1,46 @@
-print("welcome to cardrive")
+print("*********welcome to cardrive*********")
 while True:
             try:
                 source= float(input("initial reading:\n"))
                 if (source>=0):
                             des=float(input("Ending reading:\n"))
-                            if(des<source):
+                            if des<source:
+                              print("invalid")
+                            else:
                           
 
-                                distance=des-source
-                                print("select vehicle type")
-                                print("1.auto\n 2.micro\n 3.mini")
-                                type=int(input())
+                                    distance=des-source
+                                    print("select vehicle type")
+                                    print("    1.auto\n    2.micro\n    3.mini\n")
+                                    type=int(input())
 
-                                if (type==1):
-                                            print("your vehicle selected is auto")
-                                            a=(10*distance)
-                                            break
-                                            
-                                            
-                                elif (type==2):
-                                              print("your vehicle selected is micro")
-                                              b=(20*distance)
-                                              break
-                                              
-                                              
-                                elif(type==3): 
-                                              print("your vehicle selected is mini")
-                                              c=(30*distance)
-                                              break
-                                            
-                                else:
-                                              print("choice is not found")
-                            else:
-                                print("invalid")
-                                
+                                    if (type==1):
+                                                print("your vehicle selected is auto\n")
+                                                a=(10*distance)
+                                                break
+                                    elif (type==2):
+                                                  print("your vehicle selected is micro\n")
+                                                  b=(20*distance)
+                                                  break
+                                    elif(type==3): 
+                                                  print("your vehicle selected is mini\n")
+                                                  c=(30*distance)
+                                                  break
+                                    else:
+                                                  print("choice is not found")
+                           
                 else:
-                      print("invalid") 
-                                  
-
+                      print("invalid reading")
             except:
                     print("invalid")
                     break
-print("------------Bill--------------- ")
-print("your travelled distance is",distance)
-print("bill no:")
-
-  
+print("-----------------Bill--------------------")
+print("your travelled distance is:",distance)
 if (type==1):
-  print("$",a)
+  print("Amount to be paid:$",a)
 elif(type==2):
-  print("$",b)
+  print("Amount to be paid:$",b)
 elif(type==3):
-  print("$",c)
+  print("Amount to be paid:$",c)
 print("      ---welcome again---      ")
-
-
+ 
